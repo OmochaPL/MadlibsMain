2e6b2cd60bbc84c0c20c99a43867e7d09dbe9d01
OmochaPL <94681221+OmochaPL@users.noreply.github.com>
2021-11-25 11:09:43 +0100

    Update propaganda.py

--- sample_madlibs/propaganda.py
+++ sample_madlibs/propaganda.py
@@ -7,8 +7,7 @@ verb2 = input("Czasownik: ")
 famous_person =  input("Twój ulubiony polityk: ")
 
 madlib = f"Życie w Polsce jest takie {adj}! Państwo sprawia że wszyscy jesteśmy szczęśliwi, ponieważ \ 
-wszyscy kooochamy {verb1}. \
-Ciągle {verb2} jak {famous_person}!"
+wszyscy kooochamy {verb1}. \Ciągle {verb2} jak {famous_person}!"
 
 #wywołanie całego tekstu 
 print(madlib)
