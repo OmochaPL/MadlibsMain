cae1857a73ab89753232e258ec92868d4e9e530f
OmochaPL <94681221+OmochaPL@users.noreply.github.com>
2021-11-25 11:52:20 +0100

    Update propaganda.py

--- sample_madlibs/propaganda.py
+++ sample_madlibs/propaganda.py
@@ -1,13 +1,12 @@
-def madlib():
+  def madlib():
 
 # user inputs
-adj = input("Przymiotnik: ")
-verb1 = input("Czasownik: ")
-verb2 = input("Czasownik: ")
-famous_person =  input("Twój ulubiony polityk: ")
+  adj = input("Przymiotnik: ")
+  verb1 = input("Czasownik: ")
+  verb2 = input("Czasownik: ")
+  famous_person =  input("Twój ulubiony polityk: ")
 
-madlib = f"Życie w Polsce jest takie {adj}! Państwo sprawia że wszyscy jesteśmy szczęśliwi, ponieważ \ 
-wszyscy kooochamy {verb1}. \Ciągle {verb2} jak {famous_person}!"
+  madlib = f"Życie w Polsce jest takie {adj}! Państwo sprawia że wszyscy jesteśmy szczęśliwi, ponieważ wszyscy kooochamy {verb1}. Ciągle {verb2} jak {famous_person}!"
 
 #wywołanie całego tekstu 
-print(madlib)
+  print(madlib)
