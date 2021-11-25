d72db57356a1998e7904b4be3928b7106e03ee24
OmochaPL <94681221+OmochaPL@users.noreply.github.com>
2021-11-25 09:42:05 +0100

    Update and rename kotek to kotek.py

---
+++ sample_madlibs/kotek.py
@@ -0,0 +1,13 @@
+def madlib();
+
+# user inputs
+
+noun1 = input("Rzeczownik: ")
+noun2 = input("Rzeczownik2: ")
+nastroj=  input("Jak się dziś czujesz?: ")
+
+madlib = f"Pan kotek był chory i leżał w {noun1},\ 
+I przyszedł {noun2} doktór: «Jak się masz koteczku!?»\
+«{nastroj}» i łapkę wyciągnął do niego."
+
+print(madlib)
