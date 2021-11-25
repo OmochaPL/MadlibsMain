4d18ffca52d79f9a79357a81801e3462e5385132
OmochaPL <94681221+OmochaPL@users.noreply.github.com>
2021-11-25 11:50:48 +0100

    Update kotek.py

--- sample_madlibs/kotek.py
+++ sample_madlibs/kotek.py
@@ -1,13 +1,11 @@
-def madlib():
+  def madlib():
 
-# user inputs
+  # user inputs
 
-noun1 = input("Rzeczownik: ")
-noun2 = input("Rzeczownik2: ")
-nastroj=  input("Jak się dziś czujesz?: ")
+  noun1 = input("Rzeczownik: ")
+  noun2 = input("Rzeczownik2: ")
+  nastroj=  input("Jak się dziś czujesz?: ")
 
-madlib = f"Pan kotek był chory i leżał w {noun1},\ 
-I przyszedł {noun2} doktór: «Jak się masz koteczku!?»\
-«{nastroj}» i łapkę wyciągnął do niego."
+  madlib = f"Pan kotek był chory i leżał w {noun1}, I przyszedł {noun2} doktór: «Jak się masz koteczku!?»«{nastroj}» i łapkę wyciągnął do niego."
 
-print(madlib)
+  print(madlib)
