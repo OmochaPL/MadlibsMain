4493a10eddc3cdfeddb062521dd91ecb463db0ec
OmochaPL <94681221+OmochaPL@users.noreply.github.com>
2021-11-25 11:49:44 +0100

    Update RandomMadlibs.py

--- RandomMadlibs.py
+++ RandomMadlibs.py
@@ -1,6 +1,6 @@
-from sample_madlibs import propaganda, kotek, pogoda
-import random 
+  from sample_madlibs import propaganda, kotek, pogoda
+  import random 
 
-if __name__ == "__main__":
-  m = random.choice([propaganda, kotek, pogoda ])
-  m.madlib()
+  if __name__ == "__main__":
+    m = random.choice([propaganda, kotek, pogoda ])
+    m.madlib()
