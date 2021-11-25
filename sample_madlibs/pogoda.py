f7ec699784539404e42557d04fc51976c4e7efd7
OmochaPL <94681221+OmochaPL@users.noreply.github.com>
2021-11-25 11:51:38 +0100

    Update pogoda.py

--- sample_madlibs/pogoda.py
+++ sample_madlibs/pogoda.py
@@ -1,13 +1,11 @@
-def madlib():
+  def madlib():
 
 # user inputs
-month = input("Miesiąc: ")
-adj = input("Przymiotnik: ")
-animal = input("Zwierzęta: ")
+  month = input("Miesiąc: ")
+  adj = input("Przymiotnik: ")
+  animal = input("Zwierzęta: ")
 
-madlib = f"{month} zaskoczy nas swoją dynamiczną pogodą.\
-W weekend pogoda zacznie się zmieniać i dla niektórych może być {adj}.\
-Oznacza to, że trzeba być przygotowanym na ulewy, {animal}, ale też silne porywy wiatru."
+  madlib = f"{month} zaskoczy nas swoją dynamiczną pogodą. W weekend pogoda zacznie się zmieniać i dla niektórych może być {adj}. Oznacza to, że trzeba być przygotowanym na ulewy, {animal}, ale też silne porywy wiatru."
 
 #wywołanie całego tekstu 
-print(madlib)
+  print(madlib)
